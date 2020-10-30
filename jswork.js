6cbea5abdce6e37525887563da339a2506101770
Mukolas <mu.mukolas@gmail.com>
2020-10-30 16:40:43 +0200

    Initial commit

---
+++ jswork.js
@@ -0,0 +1,154 @@
+//
+//Zavdannya1
+//
+// let arr = [1,3,2,5,223];
+// let arr2 = ['one','two','three','qwerty','1']
+// let arr3 = [33,1,'two',true,false,'Robert'];
+// console.log(arr,arr2,arr3);
+//
+//Zavdannya2
+//
+// let arr =[];
+// arr[0]=1;
+// arr[1]='lel';
+// arr[2]=false;
+// console.log(arr);
+//
+//Zavdannya3
+//
+// for(let i=0;i<10;i++){
+// document.write("<div>qwdjhjkqshdkj</div>");
+// }
+//
+//Zavdannya4
+//
+// for(let i=0;i<10;i++){
+//      document.write(`<div>${i}qwdjhjkqshdkj</div>`);
+// }
+//
+//Zavdannya5
+//
+// let text= "<h1>qwerty<h1>";
+// let i=0;
+// while(i<20){
+//     document.write(text);
+//     i++;
+// }
+//
+//Zavdannya6
+//
+// let text= "<h1>qwerty<h1>";
+// let i=0;
+// while(i<20){
+//     document.write(`<h1>${i} qwerty<h1>`);
+//     i++;
+// }
+//
+//Zavdannya7
+//
+// let arr = [7,23,21,5435,123,4324,21,43,54,2];
+// for(let i=0;i<arr.length;i++){
+// console.log(arr[i]);
+// }
+//
+//Zavdannya8
+//
+// let arr = [false,"rt",34,"f",true,"wqewqe",223,"lok","adam","2"];
+// for(let i=0;i<arr.length;i++){
+// console.log(arr[i]);
+// }
+//
+//Zavdannya9
+//
+// let arr = [false,"rt",34,"f",true,"wqewqe",223,"lok",false,"2"];
+// for(let i=0; i<arr.length;i++){
+//     if(typeof arr[i] == "boolean"){
+//         console.log(arr[i]);
+//     }
+// }
+//
+//Zavdannya10
+//
+// let arr = [false,"rt",34,"f",true,"wqewqe",223,"lok",false,"2"];
+// for(let i=0; i<arr.length;i++){
+//     if(typeof arr[i] == "number"){
+//         console.log(arr[i]);
+//     }
+// }
+//
+//Zavdannya11
+//
+// let arr = [false,"rt",34,"f",true,"wqewqe",223,"lok",false,"2"];
+// for(let i=0; i<arr.length;i++){
+//     if(typeof arr[i] == "string"){
+//         console.log(arr[i]);
+//     }
+// }
+//
+//Zavdannya12
+//
+// let arr =[];
+// arr[0]=1;
+// arr[1]='lel';
+// arr[2]=false;
+// arr[3]=33;
+// arr[4]=49;
+// arr[5]=true;
+// arr[6]=88;
+// arr[7]='lets';
+// arr[8]=false;
+// arr[9]=999;
+// arr[10]='okey';
+// for(i=0;i<arr.length;i++){
+// console.log(arr[i]);
+// }
+//
+//Zavdannya13
+//
+// for(i=0;i<10;i++){
+//     console.log('step nubber: '+i);
+//     document.write("step number: "+i+" ");
+// }
+//
+//Zavdannya14
+//
+// for(i=0;i<100;i++){
+//     console.log('step nubber: '+i);
+//     document.write("step number: "+i+" ");
+// }
+//
+//Zavdannya15-17
+//
+// for(i=0;i<=100;i=i+2){
+//     console.log('step nubber: '+i);
+//     document.write("step number: "+i+" ");
+// }
+//
+//Zavdannya18
+//
+// for(i=1;i<=100;i=i+2){
+//     console.log('step nubber: '+i);
+//     document.write("step number: "+i+" ");
+// }
+//
+//Zavdannya19
+//
+// for(m=0;m<=2;m++){
+//     for(s=0;s<60;s++){
+//         document.write(' Zaraz: '+m+' hvylyna ta '+s+' secunda.'+'<br>');
+//     }    
+// }
+//
+//Zavdannya20(Зробив цікавіше)
+//
+// for(h=0;h<24;h++){
+// for(m=0;m<60;m++){
+//     document.write(h+':'+m+'<br>');
+//     if(h==2&&m==20){
+//         document.write('<h1>'+'Proyshlo '+h+' godyny ta '+m+' hvylyn'+'</h1>');
+//     }
+//     for(s=0;s<=60;s++){
+//     }
+    
+// }
+// }
